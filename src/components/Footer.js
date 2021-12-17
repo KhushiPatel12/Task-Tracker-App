95529e0ab3189154a71794b0a29d17845aebf296
KhushiPatel12 <92209863+KhushiPatel12@users.noreply.github.com>
2021-12-18 02:57:49 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; Khushi Infotech</p>
+      <Link to='/about'>About</Link>
+    </footer>
+  )
+}
+
+export default Footer
